31dbf6164981dc42eaeeb07b769c1d1173541328
Alex Murkoff <79400603+alexlnkp@users.noreply.github.com>
2024-05-19 22:52:07 +0700

    encoding works so far

--- scripts/ctconverter.py
+++ scripts/ctconverter.py
@@ -1,14 +1,7 @@
-import ctranslate2.converters as converters
 import argparse
+from pathlib import Path
 
-parser = argparse.ArgumentParser(prog="ctconverter", description="CTranslate2 converter")
-
-parser.add_argument("--load_as_float16", "-f", action="store_true", help="load model as float16", default=False)
-parser.add_argument("--quantization", "-q", type=str, choices=["int8_float16", "int8", "int16"], help="quantization mode", default="int8")
-parser.add_argument("--model", "-m", metavar="[Model Name | Model Path]", type=str, help="model name or path", default="JosephusCheung/LL7M")
-parser.add_argument("--output", "-o", metavar="[Model Name | Model Path]", type=str, help="output model name", default="models/ct2-ll7m")
-
-args = parser.parse_args()
+import ctranslate2.converters as converters
 
 """
 This is a helper script used for converting models from transformers to CTranslate2.
@@ -25,11 +18,53 @@ Usage:
     python scripts/converter.py --load_as_float16 --quantization int8_float16
 """
 
-converters.TransformersConverter(
-    args.model,
-    load_as_float16=args.load_as_float16
-).convert(
-    args.output,
-    quantization=args.quantization,
-    force=True
+parser = argparse.ArgumentParser(
+    prog="ctconverter", description="CTranslate2 converter"
 )
+
+parser.add_argument(
+    "--load_as_float16",
+    "-f",
+    action="store_true",
+    help="load model as float16",
+    default=False,
+)
+parser.add_argument(
+    "--quantization",
+    "-q",
+    type=str,
+    choices=["int8_float16", "int8_float32", "int8", "int16"],
+    help="quantization mode",
+    default="int8",
+)
+parser.add_argument(
+    "--model",
+    "-m",
+    metavar="[Model Name | Model Path]",
+    type=str,
+    help="model name or path",
+    default="JosephusCheung/LL7M",
+)
+parser.add_argument(
+    "--output",
+    "-o",
+    metavar="[Model Name | Model Path]",
+    type=str,
+    help="output model name",
+    default="models/ct2-ll7m",
+)
+
+args = parser.parse_args()
+
+output_path: str = args.output
+exec_path: str = "output"
+symlink_path: str = Path.joinpath(Path(exec_path), Path(output_path)).absolute()
+
+converters.TransformersConverter(
+    args.model, load_as_float16=args.load_as_float16
+).convert(output_path, quantization=args.quantization, force=True)
+
+Path(exec_path).joinpath(output_path.split("/")[0]).mkdir(parents=True, exist_ok=True)
+
+if not Path(symlink_path).exists(follow_symlinks=False):
+    Path(symlink_path).symlink_to(Path(output_path).absolute())
